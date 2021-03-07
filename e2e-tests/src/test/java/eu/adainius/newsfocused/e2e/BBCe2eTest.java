00e9683b93950defeee9bcb49612347c462abef4
ragutislt <lolonline>
2021-03-07 20:18:00 +0100

    e2e-tests module

---
+++ e2e-tests/src/test/java/eu/adainius/newsfocused/e2e/BBCe2eTest.java
@@ -0,0 +1,10 @@
+package eu.adainius.newsfocused.e2e;
+
+import org.junit.jupiter.api.Test;
+public class BBCe2eTest {
+
+    @Test
+    public void parses_headlines_from_BBC_and_sends_email() throws Exception {
+
+    }
+}
