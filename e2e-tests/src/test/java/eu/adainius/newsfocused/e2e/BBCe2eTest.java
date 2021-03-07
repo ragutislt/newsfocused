a69801536971b0a734a4c7f678c3f74ae0b0d644
ragutislt <lolonline>
2021-03-07 20:25:24 +0100

    Add e2e test for bbc and pass site file param to App

--- e2e-tests/src/test/java/eu/adainius/newsfocused/e2e/BBCe2eTest.java
+++ e2e-tests/src/test/java/eu/adainius/newsfocused/e2e/BBCe2eTest.java
@@ -1,10 +1,13 @@
 package eu.adainius.newsfocused.e2e;
 
 import org.junit.jupiter.api.Test;
+import eu.adainius.newsfocused.App;
+
 public class BBCe2eTest {
 
     @Test
     public void parses_headlines_from_BBC_and_sends_email() throws Exception {
-
+        String siteFile = "/sites.txt";
+        App.main(new String[] {siteFile});
     }
 }
--- newsfocused/src/main/java/eu/adainius/newsfocused/App.java
+++ newsfocused/src/main/java/eu/adainius/newsfocused/App.java
@@ -4,10 +4,9 @@ package eu.adainius.newsfocused;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String sitesFile = args[0];
+        System.out.println("Hello World! Sites will be read from: " + sitesFile);
     }
 }
