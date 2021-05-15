e915feba3d6b34dff8ae82bf52ea939166055bda
ragutislt <lolonline>
2021-05-15 15:22:36 +0200

    finish implementing should_make_each_site_parse_headlines test

--- newsfocused/src/test/java/eu/adainius/newsfocused/site/SiteListTest.java
+++ newsfocused/src/test/java/eu/adainius/newsfocused/site/SiteListTest.java
@@ -1,9 +1,12 @@
 package eu.adainius.newsfocused.site;
 
+import static eu.adainius.newsfocused.test.util.TestUtils.runWithMockedHttpResponses;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class SiteListTest {
@@ -15,9 +18,14 @@ public class SiteListTest {
     }
 
     @Test
-    public void should_make_each_site_parse_headlines() throws IOException {
-        String sitesFile = "src/test/resources/sites.txt";
-        Sites sites = new Sites(sitesFile);
-        
+    public void should_make_each_site_parse_headlines() throws Exception {
+        runWithMockedHttpResponses(() -> {
+            String sitesFile = "src/test/resources/sites.txt";
+            Sites sites = new Sites(sitesFile);
+
+            for (Site site : sites.getSites()) {
+                assertTrue(site.headlines().count() > 0);
+            }
+        });
     }
 }
