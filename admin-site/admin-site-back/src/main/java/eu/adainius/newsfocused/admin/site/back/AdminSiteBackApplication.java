e724a7d847f938da5cdc8822ecf03b97a87e662e
Aurimas <senelislt@hotmail.com>
2022-05-02 23:34:32 +0200

    Generate admin-site back spring boot app from spring initializr

---
+++ admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/AdminSiteBackApplication.java
@@ -0,0 +1,13 @@
+package eu.adainius.newsfocused.admin.site.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSiteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminSiteBackApplication.class, args);
+	}
+
+}
---
+++ admin-site/admin-site-back/src/test/java/eu/adainius/newsfocused/admin/site/back/AdminSiteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.adainius.newsfocused.admin.site.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSiteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
