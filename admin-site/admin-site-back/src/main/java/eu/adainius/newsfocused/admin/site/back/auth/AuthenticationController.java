d2386377c24b35b90297ac33afd3e0f22b45eabb
Aurimas <senelislt@hotmail.com>
2022-06-06 14:41:40 +0200

    something works

--- admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/auth/AuthenticationController.java
+++ admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/auth/AuthenticationController.java
@@ -3,13 +3,15 @@ package eu.adainius.newsfocused.admin.site.back.auth;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import org.springframework.stereotype.Component;
 
 @Component
-@Path("auth")
+@Path("/auth")
 public class AuthenticationController {
     @GET
+    @Produces("text/plain")
     public String login() {
         return "Hello";
     }
--- admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/config/ControllerConfig.java
+++ admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/config/ControllerConfig.java
@@ -3,12 +3,13 @@ package eu.adainius.newsfocused.admin.site.back.config;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import eu.adainius.newsfocused.admin.site.back.auth.AuthenticationController;
 
-@Component
-@ApplicationPath("api")
+@Configuration
+//@ApplicationPath("/api")
 public class ControllerConfig extends ResourceConfig {
 
     public ControllerConfig() {
