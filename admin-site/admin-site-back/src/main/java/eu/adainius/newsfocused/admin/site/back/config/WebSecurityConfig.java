94b8467191058674b9c5992c9158c137cdb430fa
Aurimas <senelislt@hotmail.com>
2022-06-18 13:43:24 +0200

    deny all to test

--- admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/config/WebSecurityConfig.java
+++ admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.validator.internal.util.logging.Log_.logger;
 import org.springframework.context.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -21,56 +22,71 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
+import lombok.extern.slf4j.Slf4j;
+
 @EnableWebSecurity
+@Slf4j
 public class WebSecurityConfig {
 
 	private static final String ROLE_ADMIN = "ADMIN";
 
-    @Bean
+	@Bean
 	public UserDetailsService userDetailsService() {
 		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles(ROLE_ADMIN).build());
+		manager.createUser(
+				User.withDefaultPasswordEncoder().username("user").password("password").roles(ROLE_ADMIN).build());
+		log.info("initializing userDetailsService");
 		return manager;
 	}
 
-    @Bean
-    AuthenticationEntryPoint authenticationEntryPoint() {
-        BasicAuthenticationEntryPoint authenticationEntryPoint = new BasicAuthenticationEntryPoint();
-        authenticationEntryPoint.setRealmName("Newsfocused Admin Site");
-        return authenticationEntryPoint;
-    }
-
-    @Bean
-	@Order(1)                                                        
-	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
-		http
-			.antMatcher("/admin/api/**")                             
-			.authorizeHttpRequests(authorize -> authorize
-				.anyRequest().hasRole(ROLE_ADMIN)
-			)
-			.httpBasic()
-            .authenticationEntryPoint(authenticationEntryPoint())
-            .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler());
-		return http.build();
+	@Bean
+	AuthenticationEntryPoint authenticationEntryPoint() {
+		BasicAuthenticationEntryPoint authenticationEntryPoint = new BasicAuthenticationEntryPoint();
+		authenticationEntryPoint.setRealmName("Newsfocused Admin Site");
+		return authenticationEntryPoint;
 	}
 
-    private AccessDeniedHandler accessDeniedHandler() {
-        AccessDeniedHandler handler = new AccessDeniedHandler() {
-            @Override
-            public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
-                httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
-            }
-        };
-        return handler;
-    }
+	@Bean
+	// @Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+		log.info("initializing SecurityFilterChain");
+		http. anonymous().disable()
+				.authorizeRequests().antMatchers("/**").denyAll();
+				// .authorizeRequests()
+				// .antMatchers("/admin/api/**")
+				// // .authorizeHttpRequests(authorize -> authorize
+				// // .anyRequest().hasRole(ROLE_ADMIN)
+				// // )
+				// .hasRole(ROLE_ADMIN)
+				// .and()
+				// .httpBasic()
+				// .authenticationEntryPoint(authenticationEntryPoint())
+				// .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler());
+		SecurityFilterChain securityFilterChain = http.build();
+		log.info("Security: {}", http);
+		log.info("Security: {}", securityFilterChain);
+		return securityFilterChain;
+	}
 
-	// @Bean                                                            
-	// public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
-	// 	http
-	// 		.authorizeHttpRequests(authorize -> authorize
-	// 			.anyRequest().authenticated()
-	// 		)
-	// 		.formLogin(withDefaults());
-	// 	return http.build();
+	private AccessDeniedHandler accessDeniedHandler() {
+		AccessDeniedHandler handler = new AccessDeniedHandler() {
+			@Override
+			public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+					AccessDeniedException e) throws IOException, ServletException {
+				httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+			}
+		};
+		return handler;
+	}
+
+	// @Bean
+	// public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws
+	// Exception {
+	// http
+	// .authorizeHttpRequests(authorize -> authorize
+	// .anyRequest().authenticated()
+	// )
+	// .formLogin(withDefaults());
+	// return http.build();
 	// }
 }
