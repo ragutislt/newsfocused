d337ce3d688623aabfdf23c4614287032b38d624
Aurimas <senelislt@hotmail.com>
2022-08-14 14:52:31 +0200

    serve files programatically

--- admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/config/StaticResourceServlet.java
+++ admin-site/admin-site-back/src/main/java/eu/adainius/newsfocused/admin/site/back/config/StaticResourceServlet.java
@@ -1,12 +1,20 @@
 package eu.adainius.newsfocused.admin.site.back.config;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ClassPathResource;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +28,21 @@ public class StaticResourceServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         log.info("Received a request for {}", req.getRequestURI());
-        // do something
+
+        String fileName = StringUtils.substringAfterLast(req.getRequestURI(),"static/");
+        String fileContentToServe;
+        InputStream resource = new ClassPathResource(String.format("public/static/%s", fileName)).getInputStream();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(resource))) {
+            fileContentToServe = reader.lines()
+                    .collect(Collectors.joining("\n"));
+        }
+
+        PrintWriter responseBodyWriter = resp.getWriter();
+        resp.setContentType(this.getServletContext().getMimeType(fileName));
+        resp.setCharacterEncoding("UTF-8");
+        responseBodyWriter.print(fileContentToServe);
+        responseBodyWriter.flush();   
     }
+    
 }
